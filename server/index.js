e3ff0983e92844b6e7c10ab991388cf7c5935585
Edouard Rucquoy <erucquoy@gmail.com>
2024-03-13 12:38:53 +0100

    (fix)naming: global router

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from 'cors'
-import { globalRouter } from "./features/globa.router";
+import { globalRouter } from "./features/global.router";
 
 const app = express()
 app.use(cors())
